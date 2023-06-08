b7980fed30a54a14ff00c17bcde4f07ec9bfa7f0
javimoya33 <javimoya33@gmail.com>
2023-06-08 17:55:22 +0200

    Pruebas Unitarias del controlador de Cuentas y de Cines

--- PeliculasAPITests/PruebasUnitarias/CuentasControllerTests.cs
+++ PeliculasAPITests/PruebasUnitarias/CuentasControllerTests.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿using ApiPeliculas.DTOs;
+using ManejoTareas.Controllers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +23,136 @@ namespace PeliculasAPITests.PruebasUnitarias
     [TestClass]
     public class CuentasControllerTests: BasePruebas
     {
+        [TestMethod]
         public async Task CrearUsuario()
         {
+            var nombreBD = Guid.NewGuid().ToString();
+            CrearUsuarioHelper(nombreBD);
+            var contexto2 = ConstruirContext(nombreBD);
+            var conteo = await contexto2.Users.CountAsync();
+            Assert.AreEqual(1, conteo);
+        }
 
+        [TestMethod]
+        public async Task UsuarioNoPuedeLoguearse()
+        {
+            var nombreBD = Guid.NewGuid().ToString();
+            await CrearUsuarioHelper(nombreBD);
+
+            var controller = ConstruirCuentasController(nombreBD);
+            var userInfo = new UserInfo() { Email = "ejemplo@gmail.com", Password = "malPassword" };
+            var respuesta = await controller.Login(userInfo);
+
+            Assert.IsNull(respuesta.Value);
+            var resultado = respuesta.Result as BadRequestObjectResult;
+            Assert.IsNotNull(resultado);
+        }
+
+        [TestMethod]
+        public async Task UsuarioPuedeLoguearse()
+        {
+            var nombreBD = Guid.NewGuid().ToString();
+            await CrearUsuarioHelper(nombreBD);
+
+            var controller = ConstruirCuentasController(nombreBD);
+            var userInfo = new UserInfo() { Email = "ejemplo@hotmail.com", Password = "Aa123456789!" };
+            var respuesta = await controller.Login(userInfo);
+
+            Assert.IsNotNull(respuesta.Value);
+            Assert.IsNotNull(respuesta.Value.Token);
+        }
+
+        public async Task CrearUsuarioHelper(string nombreBD)
+        {
+            var cuentasController = ConstruirCuentasController(nombreBD);
+            var userInfo = new UserInfo() { Email = "ejemplo@hotmail.com", Password = "Aa123456789!" };
+            await cuentasController.CreateUser(userInfo);
+        }
+
+        private UsuariosController ConstruirCuentasController(string nombreBD)
+        {
+            var context = ConstruirContext(nombreBD);
+            var miUserStore = new UserStore<IdentityUser>(context);
+            var userManager = TestUserManager(miUserStore);
+            var mapper = ConfigurarAutoMapper();
+
+            var httpContext = new DefaultHttpContext();
+            MockAuth(httpContext);
+            var signInManager = SetupSignInManager(userManager, httpContext);
+
+            var miConfiguracion = new Dictionary<string, string>
+            {
+                { "JWT:key", "ADKAJLKSJDLFKDJLASJDLKASJFLJDLFJDSLKFAJFKLDSFJLDSFJLSDKLFJSD" }
+            };
+
+            var configuracion = new ConfigurationBuilder()
+                .AddInMemoryCollection(miConfiguracion)
+                .Build();
+
+            return new UsuariosController(userManager, signInManager, configuracion, context, mapper);
+        }
+
+        public static UserManager<TUser> TestUserManager<TUser>(IUserStore<TUser> store = null) where TUser : class
+        {
+            store = store ?? new Mock<IUserStore<TUser>>().Object;
+            var options = new Mock<IOptions<IdentityOptions>>();
+            var idOptions = new IdentityOptions();
+            idOptions.Lockout.AllowedForNewUsers = false;
+            options.Setup(o => o.Value).Returns(idOptions);
+            var userValidators = new List<IUserValidator<TUser>>();
+            var validator = new Mock<IUserValidator<TUser>>();
+            userValidators.Add(validator.Object);
+            var pwdValidators = new List<PasswordValidator<TUser>>();
+            pwdValidators.Add(new PasswordValidator<TUser>());
+            var userManager = new UserManager<TUser>(store, options.Object, new PasswordHasher<TUser>(),
+                userValidators, pwdValidators, MockLookupNormalizer(),
+                new IdentityErrorDescriber(), null,
+                new Mock<ILogger<UserManager<TUser>>>().Object);
+            validator.Setup(v => v.ValidateAsync(userManager, It.IsAny<TUser>()))
+                .Returns(Task.FromResult(IdentityResult.Success)).Verifiable();
+            return userManager;
+        }
+
+        public static ILookupNormalizer MockLookupNormalizer()
+        {
+            var normalizerFunc = new Func<string, string>(i =>
+            {
+                if (i == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    return Convert.ToBase64String(Encoding.UTF8.GetBytes(i)).ToUpperInvariant();
+                }
+            });
+            var lookupNormalizer = new Mock<ILookupNormalizer>();
+            lookupNormalizer.Setup(i => i.NormalizeName(It.IsAny<string>())).Returns(normalizerFunc);
+            lookupNormalizer.Setup(i => i.NormalizeEmail(It.IsAny<string>())).Returns(normalizerFunc);
+            return lookupNormalizer.Object;
+        }
+
+        private static SignInManager<TUser> SetupSignInManager<TUser>(UserManager<TUser> manager,
+            HttpContext context, ILogger logger = null, IdentityOptions identityOptions = null,
+            IAuthenticationSchemeProvider schemeProvider = null) where TUser : class
+        {
+            var contextAccessor = new Mock<IHttpContextAccessor>();
+            contextAccessor.Setup(a => a.HttpContext).Returns(context);
+            identityOptions = identityOptions ?? new IdentityOptions();
+            var options = new Mock<IOptions<IdentityOptions>>();
+            options.Setup(a => a.Value).Returns(identityOptions);
+            var claimsFactory = new UserClaimsPrincipalFactory<TUser>(manager, options.Object);
+            schemeProvider = schemeProvider ?? new Mock<IAuthenticationSchemeProvider>().Object;
+            var sm = new SignInManager<TUser>(manager, contextAccessor.Object, claimsFactory, options.Object, null, schemeProvider, new DefaultUserConfirmation<TUser>());
+            sm.Logger = logger ?? (new Mock<ILogger<SignInManager<TUser>>>()).Object;
+            return sm;
+        }
+
+        private Mock<IAuthenticationService> MockAuth(HttpContext context)
+        {
+            var auth = new Mock<IAuthenticationService>();
+            context.RequestServices = new ServiceCollection().AddSingleton(auth.Object).BuildServiceProvider();
+            return auth;
         }
     }
 }
---
+++ PeliculasAPITests/PruebasUnitarias/SalasDeCineControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using ApiPeliculas.Controllers;
+using ApiPeliculas.DTOs;
+using ApiPeliculas.Entidades;
+using NetTopologySuite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeliculasAPITests.PruebasUnitarias
+{
+    public class SalasDeCineControllerTests: BasePruebas
+    {
+        [TestMethod]
+        public async Task ObtenerSalasDeCineA5KilometrosOMenos()
+        {
+            var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+
+            using (var context = LocalDbDatabaseInitializer.GetDbContextLocalDb(false))
+            {
+                var salasDeCine = new List<SalaDeCine>()
+                {
+                    new SalaDeCine()
+                    {
+                        Id = 1,
+                        Nombre = "Cine Holea",
+                        Ubicacion = geometryFactory.CreatePoint(new NetTopologySuite.Geometries.Coordinate(37.27231758753893, -6.920557848000785))
+                    }
+                };
+
+                context.AddRange(salasDeCine);
+                await context.SaveChangesAsync();
+            }
+
+            var filtro = new SalaDeCineCercanoFiltroDTO()
+            {
+                DistanciaEnKms = 5,
+                Latitud = 37.262738126314744,
+                Longitud = -6.933651721156828
+            };
+
+            using (var context = LocalDbDatabaseInitializer.GetDbContextLocalDb(false))
+            {
+                var mapper = ConfigurarAutoMapper();
+                var controller = new SalasDeCineController(context, mapper, geometryFactory);
+                var respuesta = await controller.Cercanos(filtro);
+                var valor = respuesta.Value;
+                Assert.AreEqual(2, valor.Count);
+            }
+        }
+    }
+}
